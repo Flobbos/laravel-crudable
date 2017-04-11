f7ce2c3b49f31d8bda88e93f29e9f11f21a946dc
Flobbo <Flobbo@192.168.4.3>
2017-04-11 11:03:57 +0200

    Fixed nullcheck error Added create and update Added withRelationships

--- src/Crudable/Crudable.php
+++ src/Crudable/Crudable.php
@@ -4,15 +4,15 @@ namespace Flobbos\Crudable;
 
 trait Crudable {
     
-    protected $relation;
-    
+    protected $relation = [];
+    protected $withHasMany,$withBelongsToMany;
     /**
      * Get a single item or collection
      * @param int $id
      * @return Model/Collection
      */
     public function get($id = null){
-        if(is_null($id)){
+        if(!is_null($id)){
             return $this->find($id);
         }
         return $this->model->with($this->relation)->get();
@@ -33,7 +33,7 @@ trait Crudable {
      * @return Model/Collection
      */
     public function getTrash($id = null){
-        if(is_null($id)){
+        if(!is_null($id)){
             return $this->getTrashedItem($id);
         }
         return $this->model->onlyTrashed()->with($this->relation)->get();
@@ -55,6 +55,44 @@ trait Crudable {
      */
     public function setRelation(array $relation){
         $this->relation = $relation;
+        return $this;
+    }
+    
+    public function create(array $data, $relationName = null){
+        $model = $this->model->create($data);
+        //check for hasMany
+        if(!is_null($this->withHasMany) && !is_null($relationName)){
+            $model->{$relationName}()->saveMany($this->withHasMany);
+        }
+        //check for belongsToMany
+        if(!is_null($this->withBelongsToMany) && !is_null($relationName)){
+            $model->{$relationName}()->sync($this->withBelongsToMany);
+        }
+        return $model;
+    }
+    
+    public function udpate(array $data){
+        return $this->model->update($data);
+    }
+    
+    public function delete($id, $hardDelete = false){
+        $model = $this->model->find($id);
+        if($hardDelete){
+            return $model->forceDelete($id);
+        }
+        return $model->delete($id);
+    }
+    
+    public function withHasMany(array $data, $relatedModel){
+        $this->withHasMany = [];
+        foreach($data as $k=>$v){
+            $this->withHasMany[] = new $relatedModel($v);
+        }
+        return $this;
+    }
+    
+    public function withBelongsToMany(array $data){
+        $this->withBelongsToMany = $data;
         return $this;
     }
     
