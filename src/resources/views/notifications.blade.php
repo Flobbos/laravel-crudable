02ccc148de1962cfb7be857f47cfe19ff19bd9bc
Flobbo <Flobbo@192.168.4.3>
2018-12-21 16:40:53 +0100

    Fixed auto discover, started adding default notifications partial

---
+++ src/resources/views/notifications.blade.php
@@ -0,0 +1,21 @@
+@if (count($errors) > 0)
+<div class="alert alert-danger" role="alert" data-dismiss="alert">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
+
+@if(session()->has('message'))
+<div class="alert alert-success" role="alert" data-dismiss="alert">
+    {{ session('message') }}
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
+@endif
